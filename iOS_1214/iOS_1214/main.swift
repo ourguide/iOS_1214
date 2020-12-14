50caa5550a6aa9295a5d10187739c1b71f4db12f
Chansik Yun <chansigi@gmail.com>
2020-12-14 16:11:24 +0900

    .

--- iOS_1214/iOS_1214/main.swift
+++ iOS_1214/iOS_1214/main.swift
@@ -68,3 +68,68 @@ extension Car {
     self.color = Color.allCases.randomElement()!
   }
 }
+
+// class - 상속
+class CarMarket {
+  let cars: [Car]
+  let capacity: Int
+
+  // 4. class 타입은 초기화메소드를 반드시 제공해야 합니다.
+  //  - 모든 프로터피를 온전하게 초기화하는 초기화 메소드를 '지정 초기화 메소드'라고 합니다.
+  //  - 파라미터 기본값을 제공하면, 불필요한 오버로딩을 방지할 수 있습니다.
+  init(cars: [Car], capacity: Int = 100) {
+    self.cars = cars
+    self.capacity = capacity
+  }
+
+  convenience init(names: [String]) {
+    var cars = [Car]()
+    for name in names {
+      cars.append(Car(name: name))
+    }
+
+    // 5. 지정 초기화 메소드로 위임할 수 있습니다. - 편의 초기화 메소드
+    // self.cars = cars
+    // self.capacity = 100
+    self.init(cars: cars, capacity: 100)
+  }
+}
+
+// let market1 = CarMarket(cars: [], capacity: 100)
+// let market2 = CarMarket(cars: [])
+// let market3 = CarMarket(names: [
+//  "Sonata",
+//  "Santafe",
+//  "Avante"
+// ])
+
+class OnlineCarMarket: CarMarket {
+  let url: String
+
+  init(cars: [Car], capacity: Int = 100, url: String) {
+    self.url = url
+
+    super.init(cars: cars, capacity: capacity)
+  }
+
+  // 8. 부모의 지정 초기화 메소드를 오버라이딩 하면 됩니다.
+  // 9. 부모의 지정 초기화 메소드를 자식 클래스의 지정 초기화 메소드를 호출하는 편의 초기화 메소드로 만들면 편리합니다.
+  override convenience init(cars: [Car], capacity: Int = 100) {
+    // self.url = "https://car.naver.com/"
+    // super.init(cars: cars, capacity: capacity)
+
+    self.init(cars: cars, capacity: capacity, url: "https://car.naver.com/")
+  }
+}
+
+// 6. 자식 클래스가 초기화 메소드를 제공하지 않을 경우, 부모의 지정 초기화 메소드와 편의 초기화 메소드를 사용할 수 있다.
+let market1 = OnlineCarMarket(cars: [], capacity: 100)
+let market2 = OnlineCarMarket(cars: [])
+let market3 = OnlineCarMarket(names: [
+  "Sonata",
+  "Santafe",
+  "Avante"
+])
+
+// 7. 자식이 초기화 메소드를 제공할 경우, 부모의 초기화 메소드(지정, 편의)는 사용할 수 없다,
+let market4 = OnlineCarMarket(cars: [], capacity: 100, url: "https://car.naver.com")
