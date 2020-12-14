45465ca65edc827c057ab129cddd586e24a076ce
Chansik Yun <chansigi@gmail.com>
2020-12-14 10:14:53 +0900

    .

--- iOS_1214/iOS_1214/main.swift
+++ iOS_1214/iOS_1214/main.swift
@@ -1,76 +1,48 @@
 
 import Foundation
 
-// enum
-// : 스위프트의 핵심 도구입니다.
+// enum - 타입 안정성
 
-// 채팅 메세지
-//  1. 일반적인 텍스트 메시지
-//  2. 채팅 참가 메세지
-//  3. 채팅 탈퇴 메세지
+// 1. Any: 모든 타입을 참조할 수 있는 타입입니다.
+//  1) as: 타입 변환
+//  2) is: 타입 체크
 
-#if false
-struct Message {
-  let userId: Int
-  let contents: String?
-  let date: Date
+// let arr: Array<Any> = [
 
-  let hasJoined: Bool
-  let hasLeft: Bool
-}
+let arr: [Any] = [
+  Date(),
+  "Hello",
+  100,
+  3.14
+]
 
-let joinMessage = Message(userId: 1, contents: nil, date: Date(), hasJoined: true, hasLeft: false)
-let textMessage = Message(userId: 1, contents: "Hello", date: Date(), hasJoined: false, hasLeft: false)
-let leftMessage = Message(userId: 1, contents: nil, date: Date(), hasJoined: false, hasLeft: true)
-
-// 잘못된 상태의 객체가 만들어질 수 있습니다.
-let wrongMessage = Message(userId: 1, contents: "Hi", date: Date(), hasJoined: true, hasLeft: true)
-#endif
-
-// 해결 방법 - enum
-// - '상호 베타적'인 관계를 표현할 수 있습니다.
-// - 연관 값(튜플)을 이용하면, 데이터도 포함할 수 있습니다.
-
-enum Message {
-  case text(userId: Int, contents: String, date: Date)
-  case join(userId: Int, date: Date)
-  case leave(userId: Int, date: Date)
-  // case etc
-}
-
-let joinMessage = Message.join(userId: 1, date: Date())
-
-let textMessage: Message = .text(userId: 1, contents: "Hello", date: Date())
-let leftMessage: Message = .leave(userId: 1, date: Date())
-
-func logMessage(message: Message) {
-  // 모든 경우를 처리해야 합니다.
-  //  switch message {
-  //  case .text(userId: let userId, contents: let contents, date: let date):
-  //    break
-  //  case .join(userId: let userId, date: let date):
-  //    break
-  //  case .leave(userId: let userId, date: let date):
-  //    break
-  //  }
-
-  switch message {
-  case let .text(userId, contents, date):
-    print("[\(date)(\(userId))] \(contents)")
-  case let .join(userId, date):
-    print("[\(date)(\(userId))] Join")
-  case let .leave(userId, date):
-    print("[\(date)(\(userId))] Leave")
+for element in arr {
+  switch element {
+  case let v as String:
+    print("String - \(v)")
+  case let v as Date:
+    print("Date - \(v)")
+  case let v as Int:
+    print("Int - \(v)")
+  case let v as Double:
+    print("Double - \(v)")
+  default:
+    print("지원하지 않는 타입입니다.")
   }
+
+  // 타입 체크만 필요할 때
+  #if false
+  switch element {
+  case is String:
+    print("String")
+  case is Date:
+    print("Date")
+  case is Int:
+    print("Int")
+  case is Double:
+    print("Double")
+  default:
+    print("지원하지 않는 타입입니다.")
+  }
+  #endif
 }
-
-logMessage(message: joinMessage)
-logMessage(message: textMessage)
-logMessage(message: leftMessage)
-
-// 단일 항목을 처리하고 싶을 때는 if case 를 사용하면 됩니다.
-if case let .text(userId, contents, date) = textMessage {
-  print("[\(date)(\(userId))] \(contents)")
-}
-
-
