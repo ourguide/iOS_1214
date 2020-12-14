73c4c6ab222530025927a7939be5aaf5ee512c26
Chansik Yun <chansigi@gmail.com>
2020-12-14 14:58:29 +0900

    .

--- iOS_1214/iOS_1214/main.swift
+++ iOS_1214/iOS_1214/main.swift
@@ -1,155 +1,62 @@
 
 import Foundation
 
-// Optional
+#if false
+enum Membership {
+  case gold
+  case silver
+  // case bronze
+}
+
 struct User {
-  let id: Int
-  let email: String
-  let firstName: String? // Optional<String>
-  let lastName: String? // Optional<String>
+  let membership: Membership?
 }
 
-#if false
-let user = User(id: 0, email: "hello@gmail.com", firstName: "Gildong", lastName: "Hong")
+let user = User(membership: .gold)
 
-// 1) enum을 통해 Optional이 만들어져 있습니다.
-/*
- enum Optional<Wrapped> {
-   case none
-   case some(Wrapped)
- }
- */
-
-switch user.firstName {
-case let .some(value):
-  print("Firstname - \(value)")
-case .none:
-  print("Firstname에 입력이 안되었습니다.")
-}
-
-// 위의 표현을 좀더 간결하게 표현할 수 있습니다.
-switch user.lastName {
-case let value?:
-  print("Lastname - \(value)")
-case nil:
-  print("존재하지 않습니다.")
-}
-
-// 동시에 두 개 이상의 Optinal을 처리하는 것도 가능합니다.
-if let firstName = user.firstName {
-  if let lastName = user.lastName {
-    print("\(firstName) \(lastName)")
+if let membership = user.membership {
+  switch membership {
+  case .gold:
+    print("10% 할인")
+  case .silver:
+    print("5% 할인")
   }
-}
-
-if let firstName = user.firstName,
-   let lastName = user.lastName
-{
-  print("\(firstName) \(lastName)")
-}
-
-// 다른 조건과 결합해서 사용하는 것도 가능합니다.
-if let firstName = user.firstName, !firstName.isEmpty {
-  print("조건 결합: \(firstName)")
-}
-
-if let _ = user.firstName,
-   let _ = user.lastName
-{
-  print("이름이 모두 입력되었습니다.")
-}
-
-if user.firstName != nil, user.lastName != nil {
-  print("이름이 모두 입력되었습니다.")
-}
-
-#endif
-
-class Person {
-  let email: String
-  let name: String
-
-  init(email: String, name: String) {
-    self.email = email
-    self.name = name
-  }
-}
-
-let user = User(id: 0, email: "hello@gmail.com", firstName: nil, lastName: nil)
-let person = Person(email: "hello@gmail.com", name: "Gildong")
-
-print(user)
-print(person)
-
-// dump를 이용하면, 내부의 속성 값을 확인하는 데 유용합니다.
-// - dump(user)
-// - dump(person)
-
-// CustomStringConvitable 의 프로토콜을 만족하면, 사용자가 원하는 형태로 문자열 변환이 가능합니다.
-extension User: CustomStringConvertible {
-  var description: String {
-    var result = "\(email)(\(id)) - "
-
-    if let unwrappedFirstName = firstName {
-      result += unwrappedFirstName
-    }
-
-    // 스위프트에서 옵셔널 값에 대한 바인딩을 사용할 때, 동일한 이름을 통해 가리는 것이 일반적입니다.
-    if let lastName = lastName {
-      result += " \(lastName)"
-    }
-
-    return result
-  }
-}
-
-extension Person: CustomStringConvertible {
-  var description: String {
-    return "\(email) - \(name)"
-  }
-}
-
-func createWelcomeMessage(name: String) -> String {
-  return "Welcome, \(name)"
-}
-
-#if false
-extension User {
-  var displayName: String? {
-    guard let firstName = firstName, let lastName = lastName else {
-      return nil
-    }
-
-    return "\(firstName), \(lastName)"
-  }
-}
-#endif
-
-// let result = createWelcomeMessage(name: user.displayName)
-// print(result)
-
-if let displayName = user.displayName {
-  let result = createWelcomeMessage(name: displayName)
-  print(result)
 } else {
-  let result = createWelcomeMessage(name: "Customer")
-  print(result)
+  print("0% 할인")
 }
 
-// 위의 코드는 nil 병합 연산자(??)를 이용하면, 간결하게 표현할 수 있습니다.
-let displayName = user.displayName ?? "Customer"
-let result = createWelcomeMessage(name: displayName)
-print(result)
-
-extension User {
-  // '와일드카드 패턴'을 이용하면, 다양한 조합을 쉽게 제공할 수 있습니다.
-  var displayName: String? {
-    switch (firstName, lastName) {
-    case let (firstName?, lastName?): return "\(firstName), \(lastName)"
-    case let (firstName?, nil): return firstName
-    case let (nil, lastName?): return lastName
-    default:
-      return nil
-    }
-  }
+// 위의 코드를 간결하게 표현하는 방법
+//  주의사항: default를 사용하면, 컴파일러가 새로운 항목이 추가되어도 오류를 주지 않습니다.
+switch user.membership {
+case .gold?:
+  print("10% 할인")
+case .silver?:
+  print("5% 할인")
+case nil:
+  print("0% 할인")
 }
+#endif
+
+// Optional<Bool>
+//  - Dictionary
+
+let preference: [String: Bool] = [
+  "autoLogin": true,
+  // "faceIdEnabled": true,
+]
+
+// - 'nil 병합 연산자'를 이용해서 값이 없을 경우에 대한 기본값을 지정하면 됩니다.
+#if false
+if let isFaceIdEnabled = preference["faceIdEnabled"] {
+  print(isFaceIdEnabled)
+}
+#endif
+
+if preference["faceIdEnabled"] ?? false {
+  print("페이스 아이디 설정 화면")
+} else {
+  print("활성화되어 있지 않습니다.")
+}
+
+
+
