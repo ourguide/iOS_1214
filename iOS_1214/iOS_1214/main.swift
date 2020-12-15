ff68e37957d5f0115e55aabb69ac92da7000a0c7
Chansik Yun <chansigi@gmail.com>
2020-12-15 09:52:18 +0900

    .

--- iOS_1214/iOS_1214/main.swift
+++ iOS_1214/iOS_1214/main.swift
@@ -1,89 +1,85 @@
 
 import Foundation
 
-enum LogError: Error {
-  case invalidValue
+enum ParseLocationError: Error {
+  case invalidData
+  case network(String)
+  case locationDoesNotExist
 }
 
-#if false
-struct Log {
-  var values = [String]()
+struct Location {
+  let latitude: Double
+  let longitude: Double
+}
 
-  mutating func append(messages: [String]) throws {
-    for message in messages {
-      let trimmed = message.trimmingCharacters(in: .whitespacesAndNewlines)
-
-      if trimmed.isEmpty {
-        throw LogError.invalidValue
-      } else {
-        values.append(trimmed)
-      }
-    }
+func parseLocation(_ latitude: String, _ longitude: String) throws -> Location {
+  guard let latitude = Double(latitude), let longitude = Double(longitude) else {
+    // throw ParseLocationError.invalidData
+    throw ParseLocationError.network("인터넷 연결이 필요합니다.")
   }
+
+  return Location(latitude: latitude, longitude: longitude)
 }
-#endif
-
-struct Log {
-  var values = [String]()
-
-  // 2. 연산이 완료된 이후에 객체의 상태를 변경해야 한다.
-  //   - 복사본을 이용해서 연산을 수행하는 것이 좋다.
-  mutating func append(messages: [String]) throws {
-    var temp = values
-
-    for message in messages {
-      let trimmed = message.trimmingCharacters(in: .whitespacesAndNewlines)
-
-      if trimmed.isEmpty {
-        throw LogError.invalidValue
-      } else {
-        temp.append(trimmed)
-      }
-    }
-
-    values = temp
-  }
-}
-
-// 1. 오류가 발생하였을 때, 객체의 상태는 오류 발생 이전과 동일해야 한다.
-var log = Log()
 
 do {
-  try log.append(messages: [
-    "hello, world",
-    "               ",
-    "show me the money"
-  ])
-} catch {
+  let location = try parseLocation("3.14x", "4.8")
+  print(location)
+} catch let error as NSError {
   print(error)
 }
 
-print(log)
-
-
-func writeToFiles(data: [URL: String]) throws {
-  var completed = [URL]();
-  
-  // completed의 개수와 data의 개수가 동일하지 않은 경우, 기존에 처리된 모든 파일을 삭제합니다.
-  defer {
-    if completed.count != data.count {
-      
-      // 오류를 복구하는 중에 오류가 발생하면, 복구할 수 없다.
-      //  try!: 오류가 발생하였을 경우, 프로그램이 비정상 종료합니다.
-      for url in completed {
-        try! FileManager.default.removeItem(at: url)
-      }
+// 오류에 추가적인 정보를 제공할 수 있습니다.
+extension ParseLocationError: LocalizedError {
+  var errorDescription: String? {
+    switch self {
+    case .invalidData:
+      return "잘못된 데이터 형식입니다."
+    case let .network(message):
+      return "네트워크 오류 입니다. - \(message)"
+    case .locationDoesNotExist:
+      return "존재하지 않는 위치 입니다."
     }
   }
-  
-  for (url, contents) in data {
-    try contents.write(to: url, atomically: true, encoding: .utf8)
-    completed.append(url)
+
+  var failureReason: String? {
+    switch self {
+    case .invalidData:
+      return "실패 이유: 잘못된 데이터 형식입니다."
+    case let .network(message):
+      return "실패 이유: 네트워크 오류 입니다. - \(message)"
+    case .locationDoesNotExist:
+      return "실패 이유: 존재하지 않는 위치 입니다."
+    }
+  }
+
+  var recoverySuggestion: String? {
+    return "재부팅하세요."
   }
 }
 
-// 정리
-// - 함수에 오류가 발생하였을 때, 이전의 상태로 복원하는 방법
-// 1) 임시 변수를 이용해서, 연산이 완료된 후에 객체의 상태를 변경한다.
-// 2) defer를 이용해서, 오류 발생 이전 상태로 복원한다.
-// 3) 불변 객체로 설계하면, 문제가 발생하지 않습니다.
+// NSError가 출력되는 형식을 지정할 수 있습니다.
+extension ParseLocationError: CustomNSError {
+  static var errorDomain: String {
+    return "ParseLocationError"
+  }
+
+  var errorCode: Int {
+    switch self {
+    case .invalidData:
+      return 10
+    case .network:
+      return 11
+    case .locationDoesNotExist:
+      return 12
+    }
+  }
+
+  var errorUserInfo: [String: Any] {
+    return [
+      NSLocalizedDescriptionKey: errorDescription ?? "",
+      NSLocalizedFailureReasonErrorKey: failureReason ?? "",
+      NSLocalizedRecoverySuggestionErrorKey: recoverySuggestion ?? "",
+      "Hello": 42,
+    ]
+  }
+}
