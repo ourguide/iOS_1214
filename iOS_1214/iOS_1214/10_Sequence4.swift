1ff7fd3bca5c5375d9e664e9a5ae2747e9eda0f8
Chansik Yun <chansigi@gmail.com>
2020-12-15 17:23:59 +0900

    .

--- iOS_1214/iOS_1214/10_Sequence4.swift
+++ iOS_1214/iOS_1214/10_Sequence4.swift
@@ -3,7 +3,7 @@ import Foundation
 
 //                Sequence
 //                    |
-//                Collection
+//                Collection - []
 //                    |
 //      +-------------+------------------------------------+
 //      |                     |                            |
