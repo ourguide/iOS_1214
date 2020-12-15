10432ef425e60b79a0e22b91980a164ca5e76aa7
Chansik Yun <chansigi@gmail.com>
2020-12-15 13:49:42 +0900

    .

--- iOS_1214/iOS_1214/main.swift
+++ iOS_1214/iOS_1214/main.swift
@@ -36,6 +36,7 @@ class World<C: Country> {
 }
 #endif
 
+// 런타임 프로토콜은 프로토콜을 만족하는 다양한 타입을 한번에 모아서 관리할 수 있다.
 class World {
   var countries: [Country]
 
@@ -48,15 +49,15 @@ class World {
   }
 }
 
- let countries: [Country] = [
+let countries: [Country] = [
   Korea(),
   America(),
- ]
-//let countries = [
+]
+// let countries = [
 //  America(),
 //  America(),
 //  America(),
-//]
+// ]
 
 for c in countries {
   print("\(c.name) - \(c.exchangeRate)")
@@ -66,3 +67,36 @@ let world = World(countries: countries)
 
 print(type(of: world))
 print(type(of: world.countries))
+
+func processCountryRuntime(country: Country, completion: (Country) -> Void) {
+  print("\(country.name) - \(country.exchangeRate)")
+
+  completion(country)
+}
+
+// Generic 기반으로 처리하면, 불필요한 캐스팅을 없앨 수 있다.
+func processCountryCompile<T: Country>(country: T, completion: (T) -> Void) {
+  print("\(country.name) - \(country.exchangeRate)")
+  completion(country)
+}
+
+extension Korea {
+  func hangul() {
+    print("한글")
+  }
+}
+
+processCountryRuntime(country: Korea(), completion: { (korea: Country) in
+  // 프로토콜을 만족하는 타입의 고유한 기능을 이용하기 위해서는 캐스팅이 필요하다.
+  if let korea = korea as? Korea {
+    korea.hangul()
+  }
+})
+
+processCountryCompile(country: Korea()) { (korea: Korea) in
+  korea.hangul()
+}
+
+// 결론
+// - 런타임 프로토콜은 프로토콜 만족하는 다양한 타입을 한번에 관리할 수 있다.
+// - 컴파일타임 프로토콜은 구체적인 타입에 대한 기능을 별도의 캐스팅이 없이 사용 가능하다.
