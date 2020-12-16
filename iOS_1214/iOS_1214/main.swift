8c28b1b113f4c0a22e58ede9c7dbbd4b907e8ec7
Chansik Yun <chansigi@gmail.com>
2020-12-16 15:03:33 +0900

    .

--- iOS_1214/iOS_1214/main.swift
+++ iOS_1214/iOS_1214/main.swift
@@ -18,6 +18,8 @@ load(url: "https://google.com") { result in
 }
 #endif
 
+// Never: 인스턴스화가 불가능한 타입
+//  - 실패 불가능한 Result를 표현할 때 사용됩니다.
 func load(url: String, completion: (Result<String, Never>) -> Void) {}
 
 load(url: "https://google.com") { result in
@@ -25,4 +27,9 @@ load(url: "https://google.com") { result in
   case let .success(data):
     print(data)
   }
+}
+
+//  - 함수의 반환이 불가능한 경우 사용됩니다.
+func foo() -> Never {
+  fatalError("xxx")
 }
