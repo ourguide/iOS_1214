f2a3aeec9436e273f397115492ad8262d65392de
Chansik Yun <chansigi@gmail.com>
2020-12-17 13:47:23 +0900

    .

--- SampleApp/SampleApp/ViewController.swift
+++ SampleApp/SampleApp/ViewController.swift
@@ -1,12 +1,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  @IBOutlet var imageView: UIImageView!
+  @IBOutlet var timeLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+      self.timeLabel.text = "\(Date().timeIntervalSince1970)"
+    }
   }
-
-
+  
+  @IBAction func onLoad(_ sender: UIButton) {}
 }
 
