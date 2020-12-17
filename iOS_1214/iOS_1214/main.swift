0da1703c99119a272da652e7381970360d5365cd
Chansik Yun <chansigi@gmail.com>
2020-12-17 09:32:46 +0900

    .

--- iOS_1214/iOS_1214/main.swift
+++ iOS_1214/iOS_1214/main.swift
@@ -3,6 +3,15 @@ import Foundation
 // Swift Pattern
 // - Dependency Injection
 
+struct User: Codable {
+  let login: String
+  let id: Int
+  let avatarUrl: String
+  let name: String
+  let location: String
+  let email: String?
+}
+
 final class GithubAPI {
   let session: URLSession
 
@@ -30,6 +39,14 @@ api.getJSON { result in
   switch result {
   case let .success(data):
     print("Data - \(data)")
+
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+    if let user = try? decoder.decode(User.self, from: data) {
+      print(user)
+    }
+
   case let .failure(error):
     print("Error - \(error)")
   }
