1c26264dc1320f174fe78ae79c04880f6119f8ed
Chansik Yun <chansigi@gmail.com>
2020-12-17 10:33:08 +0900

    .

--- iOS_1214/iOS_1214/main.swift
+++ iOS_1214/iOS_1214/main.swift
@@ -4,36 +4,54 @@ import Foundation
 final class CachedValue<Element> {
   private let load: () -> Element
   private var lastLoaded: Date
-  
+
   private var timeToLive: Double
   private var currentValue: Element
-  
+
   public var value: Element {
     let isRefresh = abs(lastLoaded.timeIntervalSinceNow) > timeToLive
     if isRefresh {
       currentValue = load()
       lastLoaded = Date()
     }
-    
+
     return currentValue
   }
-  
+
   init(timeToLive: Double, load: @escaping () -> Element) {
     self.timeToLive = timeToLive
     self.load = load
-  
+
     currentValue = load()
     lastLoaded = Date()
   }
 }
 
 var n = 0
-let cache = CachedValue(timeToLive: 1) { () -> Int in
+let cache1 = CachedValue(timeToLive: 1) { () -> Int in
   print("값이 갱신되었습니다")
   n += 1
   return n
 }
 
+let cache2 = CachedValue(timeToLive: 1) { () -> Int in
+  print("값이 갱신되었습니다")
+  n += 1
+  return n
+}
+
+#if false
+if cache1 == cache2 {}
+
+if cache1 > cache2 {}
+
+let data: Set<CachedValue> = [
+  cache1,
+  cache2,
+]
+#endif
+
+#if false
 print(cache.value)
 print(cache.value)
 sleep(1)
@@ -42,3 +60,4 @@ print(cache.value)
 sleep(1)
 print(cache.value)
 print(cache.value)
+#endif
