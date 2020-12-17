fba325fa8fc0ed5a0cedd3386c63e6023b892a41
Chansik Yun <chansigi@gmail.com>
2020-12-17 09:39:01 +0900

    .

--- iOS_1214/iOS_1214/main.swift
+++ iOS_1214/iOS_1214/main.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 // Swift Pattern
-// - Dependency Injection
+// - Dependency Injection: 테스트 용이성
 
 struct User: Codable {
   let login: String
@@ -12,10 +12,70 @@ struct User: Codable {
   let email: String?
 }
 
+#if false
 final class GithubAPI {
   let session: URLSession
 
   init(session: URLSession) {
+    self.session = session
+  }
+
+  func getJSON(complection: @escaping (Result<Data, Error>) -> Void) {
+    let url = URL(string: "https://api.github.com/users/apple")!
+
+    let task = session.dataTask(with: url) { data, _, error in
+      if let error = error {
+        complection(.failure(error))
+      } else if let data = data {
+        complection(.success(data))
+      }
+    }
+
+    task.resume()
+  }
+}
+
+let api = GithubAPI(session: URLSession.shared)
+api.getJSON { result in
+  switch result {
+  case let .success(data):
+    print("Data - \(data)")
+
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+    if let user = try? decoder.decode(User.self, from: data) {
+      print(user)
+    }
+
+  case let .failure(error):
+    print("Error - \(error)")
+  }
+}
+
+sleep(1)
+#endif
+
+// func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+protocol Session {
+  associatedtype Task: DataTask
+
+  func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> Task
+}
+
+protocol DataTask {
+  func resume()
+}
+
+extension URLSession: Session {}
+extension URLSessionDataTask: DataTask {}
+
+final class GithubAPI<S: Session> {
+  let session: S
+
+  typealias Task = URLSessionDataTask
+
+  init(session: S) {
     self.session = session
   }
 
