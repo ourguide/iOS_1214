372ac347c4eb59d29b44c76fe57570ad00c5364f
Chansik Yun <chansigi@gmail.com>
2020-12-17 12:07:15 +0900

    .

---
+++ SampleApp/SampleApp/AppDelegate.swift
@@ -0,0 +1,13 @@
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  // Storyboard 기반 프로젝트에 해당 프로퍼티가 존재해야 합니다. - iOS 13 이전
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+}
