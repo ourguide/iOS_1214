c281c9e08e6e9e90a0cffafe86b78bd585e744a0
Chansik Yun <chansigi@gmail.com>
2020-12-17 14:08:22 +0900

    .

--- SampleApp/SampleApp/ViewController.swift
+++ SampleApp/SampleApp/ViewController.swift
@@ -14,27 +14,34 @@ class ViewController: UIViewController {
     }
   }
   
+  func loadImageFromURL(_ url: URL, completion: @escaping (UIImage?, Error?) -> Void) {
+    DispatchQueue.global().async {
+      do {
+        let data = try Data(contentsOf: url)
+        
+        if let image = UIImage(data: data) {
+          completion(image, nil)
+        } else {
+          completion(nil, NSError(domain: "Invalid data", code: 0, userInfo: [:]))
+        }
+        
+      } catch {
+        completion(nil, error)
+      }
+    }
+  }
+  
   // 2. 비동기 - GCD
   
   @IBAction func onLoad(_ sender: UIButton) {
-   
-    
-    DispatchQueue.global().async {
-      let data = try! Data(contentsOf: IMAGE_URL)
+    loadImageFromURL(IMAGE_URL) { image, _ in
       
-      if let image = UIImage(data: data) {
-        
-        DispatchQueue.main.async {
-          self.imageView.image = image
-        }
-        
+      DispatchQueue.main.async {
+        self.imageView.image = image
       }
     }
-  
-    
   }
   
-
   // 1. 동기 버전
   //  문제점: UI 스레드에서 오래 걸리는 작업을 수행할 경우,
   //        UI의 업데이트가 멈추는 문제가 발생합니다.
