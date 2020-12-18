1ff07644f06b8d82a5e09946bb36e2245dbecda1
Chansik Yun <chansigi@gmail.com>
2020-12-18 12:18:24 +0900

    .

--- SampleApp/SampleApp/ViewController3.swift
+++ SampleApp/SampleApp/ViewController3.swift
@@ -1,15 +1,17 @@
 
+import RxCocoa
 import UIKit
 
 class ViewController3: UIViewController {
+  @IBOutlet var emailField: UITextField!
+  @IBOutlet var passwordField: UITextField!
+
+  @IBOutlet var loginButton: UIButton!
+  @IBOutlet var imageView: UIImageView!
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
     // Do any additional setup after loading the view.
   }
- 
-  
-  
-  
-  
 }
