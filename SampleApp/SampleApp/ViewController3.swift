c69f6a63b6ccdb20512dc8a48844ade581e613f9
Chansik Yun <chansigi@gmail.com>
2020-12-18 14:04:48 +0900

    .

--- SampleApp/SampleApp/ViewController3.swift
+++ SampleApp/SampleApp/ViewController3.swift
@@ -17,10 +17,20 @@ class ViewController3: UIViewController {
   
   let disposeBag = DisposeBag()
   
+  // UIKit- rx
+  //     : UI 부분을 업데이트 하거나, UI를 변경 하는 목적으로 사용된다.
+  //     -  Main 스레드에서 동작해야 한다.
+  //     -  오류 처리가 필요 없는 경우가 많다.
+  
+  //      => Driver(Observable인데, MainThread에서 동작하고, 오류가 발생하지 않는다)
+  //       - subscribe(onNext: )  => drive(onNext: )
+  //       - bind(to: )           => drive()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
     let emailIsValid = emailField.rx.text
+      .asDriver()
       .map { email -> Bool in
         guard let email = email, email.count > 5, email.contains("@") else {
           return false
@@ -30,6 +40,44 @@ class ViewController3: UIViewController {
       }
     
     let passwordIsValid = passwordField.rx.text
+      .asDriver()
+      .map { password -> Bool in
+        guard let password = password, password.count > 3 else {
+          return false
+        }
+        
+        return true
+      }
+    
+    Driver.combineLatest(emailIsValid, passwordIsValid)
+      .map { $0 && $1 }
+      .drive(loginButton.rx.isEnabled)
+      .disposed(by: disposeBag)
+    
+    emailIsValid
+      .map { !$0 }
+      .drive(imageView.rx.isHidden)
+      .disposed(by: disposeBag)
+  }
+  
+  #if false
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    let emailIsValid = emailField.rx.text
+      .catchAndReturn(nil)
+      .observe(on: MainScheduler.instance)
+      .map { email -> Bool in
+        guard let email = email, email.count > 5, email.contains("@") else {
+          return false
+        }
+        
+        return true
+      }
+    
+    let passwordIsValid = passwordField.rx.text
+      .catchAndReturn(nil)
+      .observe(on: MainScheduler.instance)
       .map { password -> Bool in
         guard let password = password, password.count > 3 else {
           return false
@@ -39,11 +87,18 @@ class ViewController3: UIViewController {
       }
     
     Observable.combineLatest(emailIsValid, passwordIsValid)
+      .observe(on: MainScheduler.instance)
       .map { $0 && $1 }
       .bind(to: loginButton.rx.isEnabled)
       .disposed(by: disposeBag)
     
-    
+    emailIsValid
+      .observe(on: MainScheduler.instance)
+      .map {
+        !$0
+      }
+      .bind(to: imageView.rx.isHidden)
+      .disposed(by: disposeBag)
     
     #if false
     Observable.combineLatest(emailIsValid, passwordIsValid)
@@ -64,4 +119,5 @@ class ViewController3: UIViewController {
       .disposed(by: disposeBag)
     #endif
   }
+  #endif
 }
