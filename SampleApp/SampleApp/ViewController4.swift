32cfc966a88656538ead79bbeb25fd31c6917d0c
Chansik Yun <chansigi@gmail.com>
2020-12-18 14:53:02 +0900

    .

--- SampleApp/SampleApp/ViewController4.swift
+++ SampleApp/SampleApp/ViewController4.swift
@@ -14,9 +14,24 @@ class ViewController4: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-
   }
   
   
-  
 }
+
+// iOP App
+// - MVC
+//   View    Controller     Model
+
+//  문제점
+// 1. View / Controller => UIKit에 의존성이 있습니다.
+//                      : 단위 테스트 작성이 어렵습니다.
+
+// 2. Controller의 역활이 크다.
+//     => 거대한 클래스의 문제가 발생한다.
+
+// - MVVM + Data Binding(RxCocoa / RxSwift)
+//   : View에 대한 변경과, 이벤트 처리 => ViewModel
+//  View        ->   UIKit
+//  ViewModel   ->   UIKit에 의존성을 가지면 안됩니다.
+//  Model
